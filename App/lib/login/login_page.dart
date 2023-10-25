8d2ddb965ed0fbc3cc1f345e96396f71d6777bc6
Flavia Gouvea <44976187+flaviagouvea@users.noreply.github.com>
2023-10-25 19:19:03 -0300

    Update login_page.dart

--- App/lib/login/login_page.dart
+++ App/lib/login/login_page.dart
@@ -1,23 +1,90 @@
-import 'package:app/login/login_page.dart';
+import 'package:app/home/lab_home_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController loginController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 166, 0)),
-        useMaterial3: true,
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Align(
+          alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Login'
+                    ),
+                    controller: loginController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                       return 'Please enter your login';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Password'
+                    ),
+                    controller: passwordController,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                       return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: ElevatedButton(
+                    child: const Text('Login'),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        if (loginController.text == "test@gmail.com" &&
+                                      passwordController.text == "test123") {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LabHomePage(
+                                login: loginController.text,
+                              )),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Invalid Credentials')),
+                            );
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Please fill input')),
+                        );
+                      }
+                    },
+                  ),
+                ),
+            ]),
+          ),
       ),
-      home: LoginPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
