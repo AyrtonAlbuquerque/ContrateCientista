2a8e1b15643d4c80ec70236f379d9d3195f7fec5
Flavia Gouvea <44976187+flaviagouvea@users.noreply.github.com>
2023-10-25 19:18:44 -0300

    Update lab_home_page.dart

--- App/lib/home/lab_home_page.dart
+++ App/lib/home/lab_home_page.dart
@@ -1,75 +1,68 @@
+import 'package:app/demands/demands_page.dart';
+import 'package:app/settings/settings_page.dart';
 import 'package:flutter/material.dart';
 
 class LabHomePage extends StatelessWidget {
-  LabHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Demandas',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Configurações',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    // setState(() {
-    //   _selectedIndex = index;
-    // });
-  }
+  LabHomePage({Key? key, required this.login}) : super(key: key);
+  final String login;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: const Text('Laboratório'),
+        backgroundColor: const Color.fromARGB(255, 255, 166, 0),
       ),
-      body: Center(
-        child: _widgetOptions[_selectedIndex],
+      body: const Center(
+        child: Text("HomePage"),
       ),
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color:Color.fromARGB(255, 255, 166, 0),
-              ),
-              child: Text('Lab Name'),
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          const UserAccountsDrawerHeader(
+            accountEmail: Text("user@mail.com"),
+            accountName: Text("Seu zé"),
+            currentAccountPicture: CircleAvatar(
+              child: Text("SZ"),
             ),
-            ListTile(
-              title: const Text('Demandas'),
-              selected: _selectedIndex == 0,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(0);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.home,
             ),
-            ListTile(
-              title: const Text('Configurações'),
-              selected: _selectedIndex == 1,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(1);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+            title: const Text('Demandas'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DemandsPage(
+                    title: 'Demands',
+                  )),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.train,
             ),
-          ],
-        ),
+            title: const Text('Configurações'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SettingsPage(
+                    title: 'Configurações',
+                  )),
+              );
+            },
+          ),
+        ],
       ),
+    ),
     );
   }
 }
